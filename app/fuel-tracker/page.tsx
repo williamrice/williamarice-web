7d68896f63a04f19463a6925db72cbe24ad48aff
William Rice <billyrice12@live.com>
2024-01-26 04:54:30 -0500

    fixed build error, i think

--- app/fuel-tracker/page.tsx
+++ app/fuel-tracker/page.tsx
@@ -5,38 +5,12 @@ import { PrismaClient } from "@prisma/client";
 import { FuelTransactionAddForm } from "./components/FuelTransactionAddForm";
 const prisma = new PrismaClient();
 
-export const FuelTracker = async () => {
-  const user = await getServerSession();
-
-  if (!user) {
-    return <div>You must be logged in to use this feature</div>;
-  }
-
-  console.log(user);
-  async function getFuelTransactions() {
-    "use server";
-
-    const fuelTransactions = await prisma.user.findUnique({
-      where: {
-        email: user?.user?.email as string,
-      },
-      include: {
-        fuelTransactions: true,
-      },
-    });
-    return fuelTransactions;
-  }
-  const fuelTransactions = await getFuelTransactions();
-  console.log(fuelTransactions);
+export const FuelTrackerPage = () => {
   return (
     <div className="flex justify-center mx-10">
-      {fuelTransactions?.fuelTransactions.length === 0 && (
-        <div>No fuel transactions found</div>
-      )}
-
       <FuelTransactionListDisplay />
     </div>
   );
 };
 
-export default FuelTracker;
+export default FuelTrackerPage;
