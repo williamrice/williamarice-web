c95ac09cc0b17be1ef6d730e642d47ca47753b8a
William Rice <billyrice12@live.com>
2024-08-01 21:49:38 -0400

    added shadow for white/white scenarios

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -49,7 +49,7 @@ export default function Navbar({ fixed }: NavBarProps) {
   ];
   return (
     <>
-      <div className="relative flex flex-wrap items-center justify-between px-2 py-3  mb-3">
+      <div className="relative flex flex-wrap items-center justify-between px-2 py-3 shadow-md">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Image
