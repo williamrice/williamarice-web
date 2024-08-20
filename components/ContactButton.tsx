2a251684a6f10a2eee77e499ab8c246144b1bdc2
William Rice <billyrice12@live.com>
2024-08-19 20:18:26 -0400

    fixed contact button

--- components/ContactButton.tsx
+++ components/ContactButton.tsx
@@ -8,7 +8,7 @@ const ContactButton = () => {
   const router = useRouter();
   return (
     <Button
-      className="mt-4 bg-white text-slate"
+      className="mt-4 bg-white text-black hover:bg-white hover:scale-110 transition-all ease-in-out hover:text-black"
       onClick={() => {
         router.push("/contact");
       }}
