4f4c5f8441902d6b155ac66213600426049b6655
William Rice <billyrice12@live.com>
2024-08-19 20:18:48 -0400

    added animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,10 +65,20 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        slideDown: {
+          "0%": { transform: "translateY(-50%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+        slideUp: {
+          "0%": { transform: "translateY(50%)" },
+          "100%": { transform: "translateY(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        slideDown: "slideDown 0.2s ease-in-out",
+        slideUp: "slideUp 0.4s ease-out",
       },
     },
   },
