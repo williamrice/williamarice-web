248024ee33eacb3d1313777d99bd94cd7ad1f806
William Rice <billyrice12@live.com>
2024-09-21 22:00:29 -0400

    reworked auth and sign in/out buttons

--- components/auth-helpers/Signin.tsx
+++ components/auth-helpers/Signin.tsx
@@ -1,15 +1,19 @@
-'use client';
+"use client";
 
-import { signIn } from 'next-auth/react';
-import React from 'react'
+import { signIn } from "next-auth/react";
+import React from "react";
+import { FaGoogle } from "react-icons/fa";
 
-const Signin = () =>
-{
-    return (
-        <button onClick={() => signIn()} className='bg-gray-800 hover:bg-gray-600 rounded-md p-3 mx-1'>
-            Sign In
-        </button>
-    )
-}
+const Signin = () => {
+  return (
+    <button
+      onClick={() => signIn("google", { callbackUrl: "/admin" })}
+      className="bg-gray-400 hover:bg-gray-600 rounded-md p-3"
+    >
+      <span className="mr-2 text-white text-lg">Sign In with Google</span>
+      <FaGoogle className="text-white inline" />
+    </button>
+  );
+};
 
-export default Signin
+export default Signin;
