b6767555fe0875cfbbf47dc812454eddcc09ea39
William Rice <billyrice12@live.com>
2024-09-21 22:01:23 -0400

    added skills section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import JumboTron from "@/components/JumboTron";
 import FeaturedProjects from "@/components/FeaturedProjects";
 import Image from "next/image";
+import SkillsSection from "@/components/SkillsSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full flex-1 text-center">
       <JumboTron />
+      <SkillsSection />
       <FeaturedProjects />
     </div>
   );
