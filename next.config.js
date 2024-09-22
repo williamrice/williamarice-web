8e6192d47fe40f186f16808cc78fea5595a59b5c
William Rice <billyrice12@live.com>
2024-09-21 22:01:48 -0400

    added aws remote pattern

--- next.config.js
+++ next.config.js
@@ -1,29 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-   
-  },
+  experimental: {},
   images: {
-    remotePatterns:[
+    remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**/*',
-      },{
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        port: '',
-        pathname: '/**/*',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**/*",
       },
       {
-        protocol: 'https',
-        hostname: 'github.com',
-        port: '',
-        pathname: '/**/*',
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "williamarice-web.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**/*",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
