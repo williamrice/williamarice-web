1cb45e39dfb9703a83ab94b5013f1f1614ebdc74
William Rice <billyrice12@live.com>
2024-09-26 22:56:50 -0400

    added header

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,12 +2,21 @@
 
 import ContactForm from "../../components/contact-form/ContactForm";
 import React from "react";
-
+import Header from "@/components/Header";
 const ContactPage = () => {
   return (
-    <div className="flex justify-center w-full">
-      <ContactForm />
-    </div>
+    <>
+      <Header>
+        <div className="h-full flex flex-col items-center justify-center">
+          <h1 className="lg:text-6xl text-4xl font-bold text-center text-white">
+            Contact
+          </h1>
+        </div>
+      </Header>
+      <div className="my-10 flex justify-center w-full">
+        <ContactForm />
+      </div>
+    </>
   );
 };
 
