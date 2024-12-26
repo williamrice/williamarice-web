936a17f534e6fe2149ef8565bfaf375dc7095565
William Rice <billyrice12@live.com>
2024-12-26 14:25:41 -0500

    updated about section text

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -2,16 +2,6 @@ import React from "react";
 import Image from "next/image";
 import Divider from "./Divider";
 
-const aboutMeText = `
-Hello! I'm William Rice, welcome to my little space on the web. I'm proudly from Eastern Kentucky and I grew up like most kids here as indigent and in unfavorable conditions with my parents being addicted to drugs. I overcame the odds and proudly served my community as a police officer for over 12 years and now I'm working full time as a Software Developer. I've had a lifelong passion for technology and writing code. 
-
-I know several programming languages and frameworks. My experience includes anything from writing Wordpress Plugins in PHP, building video game mods in C++ to full stack web development with React and other frameworks.
-
-When I'm not coding, you can find me hiking in the Red River Gorge, working out at the gym, or reading a tech book cramming my brain with more knowledge.
-
-I'm always excited to take on new challenges and collaborate on innovative projects. Feel free to reach out if you'd like to connect or discuss potential opportunities!
-`;
-
 const AboutSection = () => {
   return (
     <div className="w-full py-16 px-4 flex justify-center">
@@ -27,10 +17,36 @@ const AboutSection = () => {
             />
           </div>
           <div className="w-full md:w-1/2 px-10">
-            <h2 className="text-4xl font-bold text-center mb-8">About Me</h2>
+            <h2 className="text-4xl font-bold mb-8">About Me</h2>
             <Divider />
-            <p className="text-lg leading-relaxed whitespace-pre-line mb-8">
-              {aboutMeText}
+            <p className="text-lg text-left mb-8">
+              Hello! I&apos;m William Rice, welcome to my little space on the
+              web. I&apos;m proudly from Eastern Kentucky and I grew up like
+              most kids here as indigent and in unfavorable conditions with my
+              parents being addicted to drugs. I overcame the odds and proudly
+              served my community as a police officer for over 12 years and now
+              I&apos;m working full time as a Software Developer. I&apos;ve had
+              a lifelong passion for technology and writing code.
+            </p>
+            <p className="text-lg text-left mb-8">
+              I&apos;m proficient in a range of programming languages and
+              frameworks, with experience spanning WordPress plugin development
+              (PHP), video game modding (C++), and full-stack web development
+              using React and other modern frameworks. I&apos;m comfortable
+              learning new languages and frameworks as needed and don&apos;t
+              feel limited by any particular technology.
+            </p>
+            <p className="text-lg text-left mb-8">
+              When I&apos;m not writing code, you can find me hiking in the Red
+              River Gorge, working out, or reading a tech book cramming my brain
+              with more knowledge. I also serve my community as an elected City
+              Councilman.
+            </p>
+            <p className="text-lg text-left mb-8">
+              I&apos;m always excited to take on new challenges and collaborate
+              on innovative projects. I would really like to contribute to more
+              open source projects. Feel free to reach out if you&apos;d like to
+              connect or discuss potential opportunities!
             </p>
           </div>
         </div>
