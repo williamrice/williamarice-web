785bb77bcea0d2f77dddab9ea56eafa1d4e5f116
William Rice <william@williamarice.com>
2025-04-26 17:52:23 -0400

    ui: fix spacing on sign out button

--- components/auth-helpers/Signout.tsx
+++ components/auth-helpers/Signout.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const Signout = () => {
   return (
     <button
-      className="bg-gray-400 hover:bg-gray-600 rounded-md p-3"
+      className="bg-gray-400 hover:bg-gray-600 rounded-md text-white px-4 py-2 mt-4"
       onClick={() => signOut()}
     >
       Sign Out
