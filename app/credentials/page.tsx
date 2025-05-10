9d5c75daf0c7d0e9e4cbe6243506e7131a43d09d
William Rice <william@williamarice.com>
2025-05-10 14:04:58 -0400

    added capstone excellence credential

--- app/credentials/page.tsx
+++ app/credentials/page.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import Header from "@/components/Header";
 import BadgeCard from "@/components/BadgeCard";
 
+export interface Badge {
+  title: string;
+  type: "image" | "credly";
+  imageUrl?: string;
+  viewUrl?: string;
+  badgeId?: string;
+  badgeUrl?: string;
+}
+// <blockquote class="badgr-badge" style="font-family: Helvetica, Roboto, &quot;Segoe UI&quot;, Calibri, sans-serif;"><a href="https://api.badgr.io/public/assertions/nYyjsdcHTUOAySpCBig09g?identity__email=wrice22%40wgu.edu"><img width="120px" height="120px" src="https://api.badgr.io/public/assertions/nYyjsdcHTUOAySpCBig09g/image"></a><p class="badgr-badge-name" style="hyphens: auto; overflow-wrap: break-word; word-wrap: break-word; margin: 0; font-size: 16px; font-weight: 600; font-style: normal; font-stretch: normal; line-height: 1.25; letter-spacing: normal; text-align: left; color: #05012c;">WGU Capstone Excellence Award</p><p class="badgr-badge-date" style="margin: 0; font-size: 12px; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #555555;"><strong style="font-size: 12px; font-weight: bold; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #000;">Awarded: </strong>Apr 28, 2025</p><p class="badgr-badge-recipient" style="margin: 0; font-size: 12px; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #555555;"><strong style="font-size: 12px; font-weight: bold; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #000;">Awarded To: </strong><span style="display: block;"> William A Rice</span></p><p style="margin: 16px 0; padding: 0;"><a class="badgr-badge-verify" target="_blank" href="https://badgecheck.io?url=https%3A%2F%2Fapi.badgr.io%2Fpublic%2Fassertions%2FnYyjsdcHTUOAySpCBig09g%3Fidentity__email%3Dwrice22%2540wgu.edu&amp;identity__email=wrice22%40wgu.edu" style="box-sizing: content-box; display: flex; align-items: center; justify-content: center; margin: 0; font-size:14px; font-weight: bold; width: 48px; height: 16px; border-radius: 4px; border: solid 1px black; text-decoration: none; padding: 6px 16px; margin: 16px 0; color: black;" aria-label="Verify (opens in new window)">VERIFY</a></p><script async="async" src="https://wgu.badgr.com/assets/widgets.bundle.js"></script></blockquote>
+// <blockquote class="badgr-badge" style="font-family: Helvetica, Roboto, &quot;Segoe UI&quot;, Calibri, sans-serif;"><a href="https://api.badgr.io/public/assertions/MfpzNtt0QH2D2nP2PSNr8w?identity__email=wrice22%40wgu.edu"><img width="120px" height="120px" src="https://api.badgr.io/public/assertions/MfpzNtt0QH2D2nP2PSNr8w/image"></a><p class="badgr-badge-name" style="hyphens: auto; overflow-wrap: break-word; word-wrap: break-word; margin: 0; font-size: 16px; font-weight: 600; font-style: normal; font-stretch: normal; line-height: 1.25; letter-spacing: normal; text-align: left; color: #05012c;">WGU Certificate: Front-End Developer</p><p class="badgr-badge-date" style="margin: 0; font-size: 12px; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #555555;"><strong style="font-size: 12px; font-weight: bold; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #000;">Awarded: </strong>Feb 27, 2025</p><p class="badgr-badge-recipient" style="margin: 0; font-size: 12px; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #555555;"><strong style="font-size: 12px; font-weight: bold; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #000;">Awarded To: </strong><span style="display: block;"> William A Rice</span></p><p style="margin: 16px 0; padding: 0;"><a class="badgr-badge-verify" target="_blank" href="https://badgecheck.io?url=https%3A%2F%2Fapi.badgr.io%2Fpublic%2Fassertions%2FMfpzNtt0QH2D2nP2PSNr8w%3Fidentity__email%3Dwrice22%2540wgu.edu&amp;identity__email=wrice22%40wgu.edu" style="box-sizing: content-box; display: flex; align-items: center; justify-content: center; margin: 0; font-size:14px; font-weight: bold; width: 48px; height: 16px; border-radius: 4px; border: solid 1px black; text-decoration: none; padding: 6px 16px; margin: 16px 0; color: black;" aria-label="Verify (opens in new window)">VERIFY</a></p><script async="async" src="https://wgu.badgr.com/assets/widgets.bundle.js"></script></blockquote>
 // Define badge data
-const badges = [
+const badges: Badge[] = [
   {
     title: "Back-End Developer",
     type: "image" as const,
@@ -21,17 +31,25 @@ const badges = [
       "https://api.badgr.io/public/assertions/MfpzNtt0QH2D2nP2PSNr8w?identity__email=wrice22%40wgu.edu",
   },
   {
+    title: "Capstone Excellence Award",
+    type: "image" as const,
+    imageUrl:
+      "https://api.badgr.io/public/assertions/nYyjsdcHTUOAySpCBig09g/image",
+    viewUrl:
+      "https://api.badgr.io/public/assertions/nYyjsdcHTUOAySpCBig09g?identity__email=wrice22%40wgu.edu",
+  },
+  {
     title: "AWS Certified Cloud Practitioner",
     type: "credly" as const,
     badgeId: "20acc96e-29fd-4e65-9fb4-7623a80b1be1",
   },
   {
-    title: "AWS Certified Solutions Architect",
+    title: "CompTIA Project+",
     type: "credly" as const,
     badgeId: "755b3863-f406-4856-8826-648adbd2290b",
   },
   {
-    title: "AWS Certified Developer",
+    title: "CCNA: Introduction to Networks",
     type: "credly" as const,
     badgeId: "c14dd41a-037b-4570-9b96-21e642cc90fa",
   },
--- components/BadgeCard.tsx
+++ components/BadgeCard.tsx
@@ -3,17 +3,9 @@
 import React from "react";
 import Image from "next/image";
 import CredlyBadge from "./CredlyBadge";
+import { Badge } from "@/app/credentials/page";
 
-interface BadgeCardProps {
-  title: string;
-  type: "badgr" | "credly" | "image";
-  badgeId?: string;
-  badgeUrl?: string;
-  imageUrl?: string;
-  viewUrl?: string;
-  width?: number;
-  height?: number;
-}
+interface BadgeCardProps extends Badge {}
 
 export default function BadgeCard({
   title,
@@ -22,28 +14,13 @@ export default function BadgeCard({
   badgeUrl,
   imageUrl,
   viewUrl,
-  width = 330,
-  height = 191,
 }: BadgeCardProps) {
+  const width = 330;
+  const height = 191;
   return (
     <div className="shadow-sm rounded-md border-2 border-gray-300 p-6 rounded-lg shadow-md flex flex-col items-center h-full">
       <h2 className="text-xl font-semibold mb-4 text-center">{title}</h2>
       <div className="flex-grow flex items-center justify-center w-full">
-        {type === "badgr" && badgeUrl && (
-          <div
-            className="relative"
-            style={{ width: `${width}px`, height: `${height}px` }}
-          >
-            <iframe
-              src={badgeUrl}
-              title={`Badge: ${title}`}
-              style={{ width: "100%", height: "100%", border: "0px" }}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-          </div>
-        )}
-
         {type === "image" && imageUrl && (
           <div
             className="relative"
