993e5263b339b84c84be03ad676e129c330d8a16
William Rice <william@williamarice.com>
2026-01-19 12:40:07 -0500

    fix signout redirect

--- components/auth-helpers/Signout.tsx
+++ components/auth-helpers/Signout.tsx
@@ -5,7 +5,15 @@ const Signout = () => {
   return (
     <button
       className="bg-gray-400 hover:bg-gray-600 rounded-md text-white px-4 py-2 mt-4"
-      onClick={() => authClient.signOut()}
+      onClick={() =>
+        authClient.signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              window.location.href = "/";
+            },
+          },
+        })
+      }
     >
       Sign Out
     </button>
